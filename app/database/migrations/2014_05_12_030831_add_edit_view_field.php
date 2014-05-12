3c40f09d1797b6e0baa2fe93b4cfcf5ab94f3737
Simon Davies <simon@madeinkatana.com>
2014-05-12 13:07:55 +0930

    added edit view, service proveder and package for editing views.

---
+++ app/database/migrations/2014_05_12_030831_add_edit_view_field.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddEditViewField extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+            Schema::table('content_default_fields', function($table) {
+                $table->string('edit_view')->default('contents.form');
+                $table->string('edit_service_provider')->default('CMSDefault\Cms\CmsServiceProvider');
+                $table->string('edit_package')->default('cms');
+            });
+
+            Schema::table('content', function($table) {
+                $table->string('edit_view')->default('contents.form');
+                $table->string('edit_service_provider')->default('CMSDefault\Cms\CmsServiceProvider');
+                $table->string('edit_package')->default('cms');
+            });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
