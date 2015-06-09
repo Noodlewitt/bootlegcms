f0af2d3edd461e44c8d97f0b6b495c79b5233b98
Simon Davies <simon@madeinkatana.com>
2015-06-09 13:00:16 +0930

    added fix for migrations database_path missing function

--- src/Bootleg/Cms/CmsServiceProvider.php
+++ src/Bootleg/Cms/CmsServiceProvider.php
@@ -22,7 +22,10 @@ class CmsServiceProvider extends ServiceProvider {
 	    $this->publishes([__DIR__.'/../../../public' => public_path('vendor/bootleg/cms')], 'public');
 
 	    //publish the migrations:
-	    $this->publishes([__DIR__.'/../../../src//migrations/' => database_path('/migrations')], 'migrations');
+	    $this->publishes([__DIR__.'/../../../src/migrations/' => public_path('../databases/migrations')], 'migrations');
+
+	    // TODO: ^^ when we upgrade next - seems this has been fixed:
+	    //$this->publishes([__DIR__.'/../../../src//migrations/' => database_path('/migrations')], 'migrations');
 
 	    //publish the config
 	    $this->publishes([__DIR__.'/../../config/bootlegcms.php' => config_path('bootlegcms.php')]); //config
