cc6775865eb241413de9b6ee6edfd98a59d8c65b
Ryan_BC <ryan@madeinkatana.com>
2015-07-29 16:18:18 +0930

    expanding on plugin model

---
+++ src/models/Plugindefaultsetting.php
@@ -0,0 +1,11 @@
+<?php
+
+class Plugindefaultsetting extends \Eloquent {
+    protected $table = 'plugin_default_settings';
+    protected $fillable = array('plugin_id', 'name', 'value', 'field_type', 'field_parameters');
+
+    public function plugin(){
+        return $this->belongsTo('Plugin');
+    }
+
+}
