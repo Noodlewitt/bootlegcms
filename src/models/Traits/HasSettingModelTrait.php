7d346a802d129aacbdbaddce1606c691d75e0752
Ryan_BC <ryan@madeinkatana.com>
2015-08-28 13:56:17 +0930

    hassetting default value changes

--- src/models/Traits/HasSettingModelTrait.php
+++ src/models/Traits/HasSettingModelTrait.php
@@ -10,15 +10,13 @@ trait HasSettingModelTrait
     //__NAMESPACE__
 
 
-    public function getSetting($getSetting, $fallback = null, $first = false){
-        $setting_type = property_exists($this, $fallback) ? $this->{$fallback} : $this->setting;
-
+    public function getSetting($getSetting, $first = false, $fallback = null, $do_fallback = false){
+        $setting_type = $do_fallback && method_exists($this, $fallback) ? $this->{$fallback} : $this->setting;
         $settings = $setting_type->filter(function($model) use(&$getSetting){
             return $model->name === $getSetting;
         });
-
         if($settings->count() == 0){
-            if($fallback) return $this->getSetting($getSetting, $fallback); //fallback to default settings
+            if($fallback && !$do_fallback) return $this->getSetting($getSetting, false, $fallback, true); //fallback to default settings
             else return null; //if no default setting, return null
         }
         if($settings->count() > 1 && !$first){
