937e678fc024fdb35bc045ddecc5899968af6f1e
Ryan_BC <ryan@madeinkatana.com>
2015-09-30 13:23:10 +0930

    customizable title

--- src/views/layouts/nav.blade.php
+++ src/views/layouts/nav.blade.php
@@ -7,7 +7,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">BootlegCMS</a>
+        <a class="navbar-brand" href="#">{{ config('bootlegcms.cms_title', 'BootlegCMS') }}</a>
       </div>
       <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
