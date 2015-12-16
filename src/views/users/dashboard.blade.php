16ff4f72b07d7081d91241cf6b5822b15c00916e
ryan@katana <ryan@madeinkatana.com>
2015-12-16 17:37:07 +1030

    styling

--- src/views/users/dashboard.blade.php
+++ src/views/users/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends(view()->exists($cms_package.'::layouts.master') ? $cms_package.'::layouts.master' : 'cms::layouts.master')
 
 @section('main-content')
-<div class="col-md-offset-2 col-md-10">
+<div class="col-md-offset-2 col-sm-10">
     <div class="page-header row">	
             <!-- Page header, center on small screens -->
             <h1 class="col-xs-12"><i class="glyphicon glyphicon-home"></i>&nbsp;&nbsp;Dashboard</h1>
