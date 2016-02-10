f181725ac433ec509f20f7a548035e82c80ccdb3
Jarrad Littleford <jarrad@madeinkatana.com>
2016-02-10 17:06:05 +1030

    More catalogue updates

--- src/views/contents/input_types/upload.blade.php
+++ src/views/contents/input_types/upload.blade.php
@@ -84,7 +84,7 @@ $files = json_encode($files);
                 </div>
             </div>
             <div class="modal fade" id="modal_{{$niceName}}">
-              <div class="modal-dialog modal-lg">
+              <div class="modal-dialog modal-lg" style="z-index: 1000px;">
                 <div class="modal-content">
                   <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
