25862634f9d5b7a3c279d4213e7b33a4e183dd5a
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-02-26 10:14:31 +1030

    better ssl handling

--- src/models/ApplicationUrl.php
+++ src/models/ApplicationUrl.php
@@ -1,64 +1,63 @@
 <?php
 
-class ApplicationUrl extends Eloquent
-{
+class ApplicationUrl extends Eloquent {
 
-    protected $fillable = array('protocol', 'domain', 'folder');
+    protected $fillable = ['protocol', 'domain', 'folder'];
 
 
     public function application()
     {
         return $this->belongsTo('Application');
     }
-    
+
     public static function getBaseUrl()
     {
         $applicationurl = unserialize($GLOBALS['applicationurl']);
-        
-        if ($applicationurl->protocol) {
+
+        if ( $applicationurl->protocol ) {
             $url = $applicationurl->protocol;
         } else {
             $url = "//";
         }
-        if (substr($applicationurl->domain, -1) == '/' || substr($applicationurl->folder, 0, 1) == '/') {
-            $url .= $applicationurl->domain.$applicationurl->folder;
+        if ( substr($applicationurl->domain, -1) == '/' || substr($applicationurl->folder, 0, 1) == '/' ) {
+            $url .= $applicationurl->domain . $applicationurl->folder;
         } else {
             //we have to add the slash in manually.
-            $url .= $applicationurl->domain.'/'.$applicationurl->folder;
+            $url .= $applicationurl->domain . '/' . $applicationurl->folder;
         }
-        
-        return($url);
+
+        return ($url);
     }
 
     public static function getApplicationUrl($domain = '', $folder = '', $getFromSession = true, $setSession = true)
     {
         //dd($_SERVER['SERVER_NAME']);
-        if (!$domain) {
+        if ( ! $domain ) {
             $domain = ApplicationUrl::getDomain();
         }
 
-        if (!$folder) {
+        if ( ! $folder ) {
             $folder = ApplicationUrl::getFolder();
         }
 
         $prefix = static::getPrefix();
 
-               
+
         $applicationUrl = ApplicationUrl::with('application', 'application.setting', 'application.languages', 'application.plugins', 'application.url', 'application.secure_url')
             ->where('domain', '=', "$domain")
             ->where('folder', 'LIKE', $folder)
-            ->where(function($sq) use ($prefix) {
+            ->where(function ($sq) use ($prefix) {
                 $sq->where('prefix', 'LIKE', $prefix)->orWhere('prefix', '')->orWhereNull('prefix');
             })
             ->orderBy('ssl', \Request::secure() ? 'DESC' : 'ASC')
             ->orderBy('prefix', 'DESC')
             ->first();
 
-        if ($setSession && !Session::get('application_url'.$folder)) {
-            Session::put('application_url'.$folder, $applicationUrl);
+        if ( $setSession && ! Session::get('application_url' . $folder) ) {
+            Session::put('application_url' . $folder, $applicationUrl);
         }
-        
-        return($applicationUrl);
+
+        return ($applicationUrl);
     }
 
     public static function getFolder()
@@ -66,55 +65,55 @@ class ApplicationUrl extends Eloquent
         $folder = str_replace('public/index.php', '', $_SERVER['SCRIPT_NAME']);
         $folder = trim(str_replace('public/', '', $folder), '/');
         $folder = trim(str_replace('index.php', '', $folder), '/');
-        if ($folder != '/' ) {
-            $folder = '/'.$folder;
+        if ( $folder != '/' ) {
+            $folder = '/' . $folder;
         }
-        return($folder);
+
+        return ($folder);
     }
 
     public static function getPrefix()
     {
         $url = parse_url($_SERVER['REQUEST_URI']);
 
-        if(isset($url['path']))
-        {
+        if ( isset($url['path']) ) {
             $path = explode('/', $url['path']);
 
             $folder = static::getFolder();
 
-            if($folder == '/') {
-                if(isset($path[1])) return $path[1];
+            if ( $folder == '/' ) {
+                if ( isset($path[1]) ) return $path[1];
             } else {
-                if(isset($path[2])) return $path[2];
+                if ( isset($path[2]) ) return $path[2];
             }
         }
 
         return '';
     }
-    
+
     public static function getDomain()
     {
-        if(@$_SERVER['HTTP_HOST']){
-            $domain = trim($_SERVER['HTTP_HOST']);    
+        if ( @$_SERVER['HTTP_HOST'] ) {
+            $domain = trim($_SERVER['HTTP_HOST']);
         }
 
-        return(@$domain);
+        return (@$domain);
     }
 
     public function getUrlAttribute($secure = null)
     {
         $protocol = 'http://';
 
-        if($secure == null) {
+        if ( $secure == null ) {
             $protocol = \Request::secure() ? 'https://' : 'http://';
         } else {
             $protocol = $secure ? 'https://' : 'http://';
         }
 
-        if($protocol == 'https://' && (!app()->environment('production') || $this->ssl == 0)) $protocol = 'http://';
+        if ( $protocol == 'https://' && (app()->environment('local') || $this->ssl == 0) ) $protocol = 'http://';
 
         $folder = ($this->folder ? $this->folder : '/');
-        if($folder == '/') $folder = '';
+        if ( $folder == '/' ) $folder = '';
 
         return $protocol . $this->domain . $folder;
     }
--- src/views/users/dashboard.blade.php
+++ src/views/users/dashboard.blade.php
@@ -1,18 +1,17 @@
 @extends(view()->exists($cms_package.'::layouts.master') ? $cms_package.'::layouts.master' : 'cms::layouts.master')
 
 @section('main-content')
-<div class="col-md-offset-2 col-sm-10">
-    <div class="page-header row">	
+    <div class="col-md-offset-2 col-sm-10">
+        <div class="page-header row">
             <!-- Page header, center on small screens -->
             <h1 class="col-xs-12"><i class="glyphicon glyphicon-home"></i>&nbsp;&nbsp;Dashboard</h1>
+        </div>
+        <div class="row">
+            @foreach(Event::fire('dashboard.items', []) as $dashItem)
+                @if($dashItem instanceof \Illuminate\Contracts\View\View)
+                    {!! $dashItem->render() !!}
+                @endif
+            @endforeach
+        </div>
     </div>
-    <div class="row">
-        <?php
-            $dashItems = Event::fire('dashboard.items', []);
-        ?>
-        @foreach($dashItems as $dashItem)
-          {!! $dashItem->render() !!}
-        @endforeach
-    </div>
-</div>
 @stop
