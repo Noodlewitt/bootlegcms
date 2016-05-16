7136c07e1db3a73e8fcba0ea61e59880c91450ab
Simon Davies <simon@madeinkatana.com>
2016-05-16 12:14:10 +0930

    upgraded js packages and added in nicer tagging routines

--- public/components/bootstrap-tagsinput/Gruntfile.js
+++ public/components/bootstrap-tagsinput/Gruntfile.js
@@ -1,23 +1,5 @@
 module.exports = function(grunt) {
 
-  var lib = [
-    'angular/angular.min.js',
-    'bootstrap-2.3.2/docs/assets/css/bootstrap.css',
-    'bootstrap-2.3.2/docs/assets/css/docs.css',
-    'bootstrap-2.3.2/docs/assets/js/bootstrap.min.js',
-    'bootstrap-3/dist/css/bootstrap-theme.min.css',
-    'bootstrap-3/dist/css/bootstrap.min.css',
-    'bootstrap-3/dist/js/bootstrap.min.js',
-    'bootstrap-tagsinput/bootstrap-tagsinput.css',
-    'jquery/dist/jquery.min.js',
-    'rainbow/js/language/generic.js',
-    'rainbow/js/language/html.js',
-    'rainbow/js/language/javascript.js',
-    'rainbow/js/rainbow.min.js',
-    'rainbow/themes/github.css',
-    'typeahead.js/dist/typeahead.bundle.js'
-  ];
-
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
@@ -32,12 +14,7 @@ module.exports = function(grunt) {
     copy: {
       build: {
         files: [
-          {expand: true, flatten: true, src: ['src/*.*'], dest: 'dist/', filter: 'isFile'},
-
-          {expand: true, flatten: false, cwd: 'bower_components/', src: [lib], dest: 'examples/lib/'},
-          {expand: true, flatten: false, cwd: 'dist/', src: ['**'], dest: 'examples/lib/bootstrap-tagsinput'},
-
-          {expand: true, flatten: false, cwd: 'bower_components/', src: [lib], dest: 'test/lib/'}
+          {expand: true, flatten: true, src: ['src/*.*'], dest: 'dist/', filter: 'isFile'}
         ]
       }
     },
@@ -49,15 +26,14 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'dist/<%= pkg.name %>.min.js': 'src/<%= pkg.name %>.js',
-          'dist/<%= pkg.name %>-angular.min.js': 'src/<%= pkg.name %>-angular.js'
+          'dist/<%= pkg.name %>.min.js': 'dist/<%= pkg.name %>.js'
         }
       }
     },
     less: {
       build: {
         files: {
-          "dist/<%= pkg.name %>.css": "src/<%= pkg.name %>.less"
+          "dist/<%= pkg.name %>.css": "dist/<%= pkg.name %>.less"
         }
       }
     },
@@ -70,21 +46,6 @@ module.exports = function(grunt) {
         browsers: ['PhantomJS']
       }
     },
-    zip: {
-      delpoy: {
-        cwd: 'dist/',
-        src:  ['dist/bootstrap-tagsinput*.*'],
-        dest:  'dist/<%= pkg.name %>.zip'
-      }
-    },
-    jquerymanifest: {
-      options: {
-        source: grunt.file.readJSON('package.json'),
-        overrides: {
-          title: '<%= pkg.title %>'
-        }
-      }
-    },
     watch: {
       scripts: {
         files: ['src/**/*.*', 'test/**/*.js', 'examples/**/*.html'],
@@ -94,9 +55,24 @@ module.exports = function(grunt) {
           interupt: true
         }
       }
+    },
+    zip: {
+      delpoy: {
+        cwd: 'dist/',
+        src:  ['dist/bootstrap-tagsinput*.*'],
+        dest:  'build/<%= pkg.name %>.zip'
+      }
+    },
+    jquerymanifest: {
+      options: {
+        source: grunt.file.readJSON('package.json'),
+        overrides: {
+          title: '<%= pkg.title %>'
+        }
+      }
     }
   });
 
-  grunt.registerTask('build', ['test', 'jquerymanifest', 'zip']);
-  grunt.registerTask('test', ['uglify', 'less', 'copy', 'karma']);
+  grunt.registerTask('build', ['unit', 'jquerymanifest', 'copy:build', 'uglify:build', 'less:build', 'zip']);  
+  grunt.registerTask('unit', ['karma']);
 };
