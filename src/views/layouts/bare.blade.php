0a6f9d82255837e8466dbbab3ac862700849c5d5
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-04-13 10:47:29 +0930

    Secure URLs

--- src/views/layouts/bare.blade.php
+++ src/views/layouts/bare.blade.php
@@ -14,8 +14,8 @@
         @section('main-head')
             <title>BootlegCMS: {{@$application->name}}</title>
         @show
-        <script type="text/javascript" src="{{Applicationurl::getBaseUrl()}}vendor/bootleg/cms/js/script.min.js"></script>
-        <link rel="stylesheet" href="{{Applicationurl::getBaseUrl()}}vendor/bootleg/cms/css/application.css" />
+        <script type="text/javascript" src="/vendor/bootleg/cms/js/script.min.js"></script>
+        <link rel="stylesheet" href="/vendor/bootleg/cms/css/application.css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
         <?php
             $headerItems = Event::fire('html.master.header.end', array());
